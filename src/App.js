ae6bfa8f27b62f60b1873c2adb7f045d81d4c675
theDe-bugger <jaimil.d@icloud.com>
2024-05-09 22:03:46 -0400

    update bootstrap design

--- src/App.js
+++ src/App.js
@@ -2,12 +2,23 @@ import "./App.css";
 import Webcam from "react-webcam";
 import { useRef, useState, useCallback } from "react";
 import axios from "axios";
+import Button from "react-bootstrap/Button";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Alert from "react-bootstrap/Alert";
+import Image from "react-bootstrap/Image";
+
 function App() {
   const webcamRef = useRef(null);
   const [imgSrc, setImgSrc] = useState(null);
   const [trashCount, setTrashCount] = useState(0);
   const [recycleCount, setRecycleCount] = useState(0);
   const [compostCount, setCompostCount] = useState(0);
+  const [isLoading, setLoading] = useState(false);
+  const [showAlert, setShowAlert] = useState(false);
+  const [alertVariant, setAlertVariant] = useState("success");
+  const [alertMessage, setAlertMessage] = useState("");
 
   const getPrediction = async (content) => {
     const endpoint = `https://ecosort-prediction.cognitiveservices.azure.com/customvision/v3.0/Prediction/${process.env.REACT_APP_CUSTOM_VISION_ID}/classify/iterations/Iteration1/image`;
@@ -40,51 +51,112 @@ function App() {
           } else if (result === "compost") {
             setCompostCount((compostCount) => compostCount + 1);
           }
+          setAlertVariant("success");
+          setAlertMessage(
+            `This object is detected to go into the ${result} bin`
+          );
+        } else {
+          setAlertVariant("warning");
+          setAlertMessage("The API returned no prediction for some reason!");
         }
       })
       .catch((error) => {
         console.error("Error: ", error);
+        setAlertVariant("danger");
+        setAlertMessage("There was an api call error!");
       });
+
+    setShowAlert(true);
+    // setImgSrc(null);
   };
 
-  const capture = useCallback(() => {
+  const capture = useCallback(async () => {
     const imageSrc = webcamRef.current.getScreenshot();
     console.log(imageSrc);
-    getPrediction(imageSrc);
     setImgSrc(imageSrc);
-  }, [webcamRef]);
+    try {
+      setLoading(true);
+      await getPrediction(imageSrc);
+      setLoading(false);
+    } catch (error) {
+      console.error("Error processing image: ", error);
+    }
+  }, [webcamRef, getPrediction]);
 
   const retake = () => {
     setImgSrc(null);
   };
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>EcoSort</h1>
-        <p>Take a picture below to get started.</p>
-        <div className="container">
-          {imgSrc ? (
-            <img src={imgSrc} alt="webcam" />
-          ) : (
-            <Webcam
-              height={500}
-              width={500}
-              ref={webcamRef}
-              screenshotFormat="image/jpeg"
-            />
-          )}
-          <div className="btn-container">
+      <Container className="main">
+        <Row>
+          <h1>EcoSort</h1>
+          <p>Take a picture of any item to find which bin it goes into!</p>
+        </Row>
+        <Row className="alert-container">
+          <Alert
+            show={showAlert}
+            variant={alertVariant}
+            onClose={() => setShowAlert(false)}
+            dismissible
+          >
+            {alertMessage}
+          </Alert>
+        </Row>
+        <Row className="img-container">
+          <div className="img-container">
             {imgSrc ? (
-              <button onClick={retake}>Take another photo</button>
+              <Image src={imgSrc} alt="webcam" height={500} />
             ) : (
-              <button onClick={capture}>Capture photo</button>
+              <Webcam
+                height={500}
+                width={500}
+                ref={webcamRef}
+                screenshotFormat="image/jpeg"
+              />
             )}
           </div>
-          <p>Trash Count: {trashCount}</p>
-          <p>Recycle Count: {recycleCount}</p>
-          <p>Compost Count: {compostCount}</p>
-        </div>
-      </header>
+        </Row>
+        <Row>
+          <div className="btn-container">
+            {imgSrc ? (
+              <Button
+                onClick={() => {
+                  setShowAlert(false);
+                  retake();
+                }}
+                variant="primary"
+                size="lg"
+              >
+                {isLoading ? "Loading..." : "Take another photo"}
+              </Button>
+            ) : (
+              <Button onClick={capture} variant="secondary" size="lg">
+                {isLoading ? "Loading..." : "Capture photo"}
+              </Button>
+            )}
+          </div>
+        </Row>
+        <Row>
+          <Col>
+            <p>
+              Trash Count: <br></br> {trashCount}
+            </p>
+          </Col>
+          <Col>
+            <p>
+              Recycle Count: <br></br>
+              {recycleCount}
+            </p>
+          </Col>
+          <Col>
+            <p>
+              Compost Count: <br></br>
+              {compostCount}
+            </p>
+          </Col>
+        </Row>
+      </Container>
     </div>
   );
 }
